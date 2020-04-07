834b8c6b4a768ed3d94bb18649bf944e34a92dca
Steven <chaolic6505@gmail.com>
2020-04-06 23:38:59 -0700

    reset

--- venv/bin/database/mydatabase.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
-# Global Variables
-SQLITE                  = 'sqlite'
-
-# Table Names
-USERS           = 'users'
-ADDRESSES       = 'addresses'
